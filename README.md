546f2f66f9b31d31f11f8cd7e2f8978bd57cbdc0
Chapel <chaptc@gmail.com>
2017-04-08 13:40:41 -0700

    Changing boilerplate code

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-deadpixels
+# Dead Pixels mobile app
+
+Description coming soon.
+
+## Built With
+
+React Native - https://facebook.github.io/react-native/  
+Android Emulator (AVD) - https://developer.android.com/studio/run/emulator.html
+
+## Authors
+
+* **Ryan Chapel** - *Initial creation / work* - info@ryanchapel.com
--- index.ios.js
+++ index.ios.js
@@ -1,8 +1,18 @@
-import React from "react-native";
-import Root from "./src/containers/root";
+import {Provider} from 'react-redux';
+import store from './src/redux/store';
+import AppViewContainer from './src/modules/AppViewContainer';
 
-const {
-  AppRegistry
-} = React;
+import React, {Component} from 'react';
+import {AppRegistry} from 'react-native';
 
-AppRegistry.registerComponent("App", () => Root);
+class PepperoniAppTemplate extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <AppViewContainer />
+      </Provider>
+    );
+  }
+}
+
+AppRegistry.registerComponent('PepperoniAppTemplate', () => PepperoniAppTemplate);
